6d07c31af95bfe2ca20d23602f7ba4611eb8724c
MisterE87 <erwin_van_wingerden@hotmail.com>
2026-07-17 11:12:21 +0200

    Added more interaction fragments, updated a bit the categories

--- data/sounds.php
+++ data/sounds.php
@@ -3,8 +3,8 @@
 return [
     [
         'title' => 'Barbarian not thinking things through',
-        'character' => 'Fibonacci',    'occasion' => 'Combat',
-        'file' => 'assets/sounds/Combat/BarbarianIsStupid.mp3',
+        'character' => 'Fibonacci',    'occasion' => 'Brokk',
+        'file' => 'assets/sounds/Brokk/BarbarianIsStupid.mp3',
     ],
     [
         'title' => 'Craftsmanship',
@@ -12,7 +12,7 @@ return [
         'file' => 'assets/sounds/Bar/Craftsmanship.mp3',
     ],
     [
-        'title' => 'Fibonacci And Statistics',
+        'title' => 'Statistically speaking should work',
         'character' => 'Fibonacci',    'occasion' => 'Puzzle',
         'file' => 'assets/sounds/Puzzle/FibonacciAndStatistics.mp3',
     ],
@@ -30,6 +30,51 @@ return [
         'title' => 'One Ale',
         'character' => 'Fibonacci',    'occasion' => 'Bar',
         'file' => 'assets/sounds/Bar/OneAle.mp3',
+    ],
+    [
+        'title' => 'Continue',
+        'character' => 'Fibonacci',    'occasion' => 'Bar',
+        'file' => 'assets/sounds/Bar/Continue.mp3',
+    ],
+    [
+        'title' => 'Curiousity Incurable',
+        'character' => 'Fibonacci',    'occasion' => 'Bar',
+        'file' => 'assets/sounds/Bar/CuriousityIncurable.mp3',
+    ],
+    [
+        'title' => 'Fascinating',
+        'character' => 'Fibonacci',    'occasion' => 'Bar',
+        'file' => 'assets/sounds/Bar/Fascinating.mp3',
+    ],
+    [
+        'title' => 'Observe Carefully',
+        'character' => 'Fibonacci',    'occasion' => 'Bar',
+        'file' => 'assets/sounds/Bar/ObserveCarefully.mp3',
+    ],
+    [
+        'title' => 'Pleasure To Meet And Ask Question',
+        'character' => 'Fibonacci',    'occasion' => 'Bar',
+        'file' => 'assets/sounds/Bar/PleasureToMeetAndAskQuestion.mp3',
+    ],
+    [
+        'title' => 'Read About This',
+        'character' => 'Fibonacci',    'occasion' => 'Bar',
+        'file' => 'assets/sounds/Bar/ReadAboutThis.mp3',
+    ],
+    [
+        'title' => 'Remarkable Company',
+        'character' => 'Fibonacci',    'occasion' => 'Bar',
+        'file' => 'assets/sounds/Bar/RemarkableCompany.mp3',
+    ],
+    [
+        'title' => 'Tavern Is Library',
+        'character' => 'Fibonacci',    'occasion' => 'Bar',
+        'file' => 'assets/sounds/Bar/TavernIsLibrary.mp3',
+    ],
+    [
+        'title' => 'What Is Your Profession',
+        'character' => 'Fibonacci',    'occasion' => 'Bar',
+        'file' => 'assets/sounds/Bar/WhatIsYourProfession.mp3',
     ],
     [
         'title' => 'Purely Research',
@@ -68,23 +113,93 @@ return [
     ],
     [
         'title' => 'Brokk Trolot: What Are You Doing?',
-        'character' => 'Fibonacci',    'occasion' => 'Annoyed',
-        'file' => 'assets/sounds/Annoyed/BrokkTrolotWhatAreYouDoing.mp3',
+        'character' => 'Fibonacci',    'occasion' => 'Brokk',
+        'file' => 'assets/sounds/Brokk/BrokkTrolotWhatAreYouDoing.mp3',
+    ],
+    [
+        'title' => 'Behind Brokk',
+        'character' => 'Fibonacci',    'occasion' => 'Brokk',
+        'file' => 'assets/sounds/Brokk/BehindBrokk.mp3',
+    ],
+    [
+        'title' => 'Brokk Rages',
+        'character' => 'Fibonacci',    'occasion' => 'Brokk',
+        'file' => 'assets/sounds/Brokk/BrokkRages.mp3',
+    ],
+    [
+        'title' => 'Combat Calculations',
+        'character' => 'Fibonacci',    'occasion' => 'Brokk',
+        'file' => 'assets/sounds/Brokk/calculationsCombat.mp3',
+    ],
+    [
+        'title' => 'Kindness Is Not Weak',
+        'character' => 'Fibonacci',    'occasion' => 'Brokk',
+        'file' => 'assets/sounds/Brokk/kindnessnotweak.mp3',
+    ],
+    [
+        'title' => 'After drinking potion: Am I Glowing',
+        'character' => 'Fibonacci',    'occasion' => 'Thorn',
+        'file' => 'assets/sounds/Thorn/amIglowing.mp3',
+    ],
+    [
+        'title' => 'admire Curiosity, question survival strategy',
+        'character' => 'Fibonacci',    'occasion' => 'Thorn',
+        'file' => 'assets/sounds/Thorn/curiousity.mp3',
+    ],
+    [
+        'title' => 'I have Follow-up Questions',
+        'character' => 'Fibonacci',    'occasion' => 'Thorn',
+        'file' => 'assets/sounds/Thorn/follow-up-questions.mp3',
+    ],
+    [
+        'title' => 'Brilliant but might be catastrophic',
+        'character' => 'Fibonacci',    'occasion' => 'Thorn',
+        'file' => 'assets/sounds/Thorn/inventionThorn.mp3',
+    ],
+    [
+        'title' => 'If Thorn asks to trust yuo, I should not',
+        'character' => 'Fibonacci',    'occasion' => 'Thorn',
+        'file' => 'assets/sounds/Thorn/noTrust.mp3',
+    ],
+    [
+        'title' => 'Tested This on someone else?',
+        'character' => 'Fibonacci',    'occasion' => 'Thorn',
+        'file' => 'assets/sounds/Thorn/testedthis.mp3',
+    ],
+    [
+        'title' => 'Announce Please',
+        'character' => 'Fibonacci',    'occasion' => 'Wrenn',
+        'file' => 'assets/sounds/Wrenn/announcePlease.mp3',
+    ],
+    [
+        'title' => 'Known They Were There',
+        'character' => 'Fibonacci',    'occasion' => 'Wrenn',
+        'file' => 'assets/sounds/Wrenn/knowntheywerethere.mp3',
+    ],
+    [
+        'title' => 'Strategies Chosen',
+        'character' => 'Fibonacci',    'occasion' => 'Wrenn',
+        'file' => 'assets/sounds/Wrenn/strategieschosen.mp3',
+    ],
+    [
+        'title' => 'Sees Possibilities',
+        'character' => 'Fibonacci',    'occasion' => 'Wrenn',
+        'file' => 'assets/sounds/Wrenn/SeesPossibilities.mp3',
     ],
     [
         'title' => 'Jealous?',
-        'character' => 'Fibonacci',    'occasion' => 'Mean',
-        'file' => 'assets/sounds/Mean/Jealous.mp3',
+        'character' => 'Fibonacci',    'occasion' => 'Confused',
+        'file' => 'assets/sounds/Confused/Jealous.mp3',
     ],
     [
-        'title' => 'Poison',
-        'character' => 'Fibonacci',    'occasion' => 'Mean',
-        'file' => 'assets/sounds/Mean/Poison.mp3',
+        'title' => 'Poison for Brokk?',
+        'character' => 'Fibonacci',    'occasion' => 'Brokk',
+        'file' => 'assets/sounds/Brokk/Poison.mp3',
     ],
     [
         'title' => 'Dont throw me!!!',
-        'character' => 'Fibonacci',    'occasion' => 'Scared',
-        'file' => 'assets/sounds/Scared/Noooodontthrowme.mp3',
+        'character' => 'Fibonacci',    'occasion' => 'Combat',
+        'file' => 'assets/sounds/Combat/Noooodontthrowme.mp3',
     ],
     [
         'title' => 'Sleep Is Productive',
@@ -98,8 +213,8 @@ return [
     ],
     [
         'title' => 'Im not Procastinating, Im Optimizing',
-        'character' => 'Fibonacci',    'occasion' => 'Resting',
-        'file' => 'assets/sounds/Resting/ImnotProcastinatingImOptimizing.mp3',
+        'character' => 'Fibonacci',    'occasion' => 'Combat',
+        'file' => 'assets/sounds/Combat/ImnotProcastinatingImOptimizing.mp3',
     ],
     [
         'title' => 'Binary',
@@ -137,6 +252,16 @@ return [
         'file' => 'assets/sounds/Puzzle/MysterySolved.mp3',
     ],
     [
+        'title' => 'Already Solved This',
+        'character' => 'Fibonacci',    'occasion' => 'Puzzle',
+        'file' => 'assets/sounds/Puzzle/AlreadySolvedThis.mp3',
+    ],
+    [
+        'title' => 'Hypothesis',
+        'character' => 'Fibonacci',    'occasion' => 'Puzzle',
+        'file' => 'assets/sounds/Puzzle/Hypothesis.mp3',
+    ],
+    [
         'title' => 'Lets Think This Through',
         'character' => 'Fibonacci',    'occasion' => 'Puzzle',
         'file' => 'assets/sounds/Puzzle/LetsThinkThisThrough.mp3',
@@ -147,9 +272,9 @@ return [
         'file' => 'assets/sounds/Resting/Sleep.mp3',
     ],
     [
-        'title' => 'Understimated',
-        'character' => 'Fibonacci',    'occasion' => 'Resting',
-        'file' => 'assets/sounds/Resting/Understimated.mp3',
+        'title' => 'Understimated that (something goes wrong in combat)',
+        'character' => 'Fibonacci',    'occasion' => 'Combat',
+        'file' => 'assets/sounds/Combat/Understimated.mp3',
     ],
     [
         'title' => 'Improve our odds',
